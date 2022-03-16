6079c87164147fc34178724f6766f0b03a6461a3
Vedansh Bhardwaj <vedansh.ved@anywhere.co>
2022-03-16 19:40:29 +0530

    Demonstration of the use of entryset() function

---
+++ EntrySet.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class EntrySet {
+	public static void main(String [] args){
+	
+	HashMap<String, Integer> map = new HashMap<>();
+	map.put("One", 1);
+	map.put("Two", 2);
+	map.put("Three", 3);
+	map.put("Four", 4);
+	map.put("Five", 5);
+	map.put("Six", 6);
+	System.out.println("Set view of map - ");
+	System.out.println(map.entrySet());
+	}
+}
