721c77d05e07a8f283b753a2223c0aadb26219ff
Vedansh Bhardwaj <vedansh.ved@anywhere.co>
2022-03-18 21:29:01 +0530

    Updating line by line use of entrySet()

--- EntrySet.java
+++ EntrySet.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class EntrySet {
 	public static void main(String [] args){
 	
-	HashMap<String, Integer> map = new HashMap<>();
+	Map<String, Integer> map = new TreeMap<>();
 	map.put("One", 1);
 	map.put("Two", 2);
 	map.put("Three", 3);
@@ -11,5 +11,11 @@ public class EntrySet {
 	map.put("Six", 6);
 	System.out.println("Set view of map - ");
 	System.out.println(map.entrySet());
+
+	System.out.println("Printing one by one ------>");
+
+	for(Map.Entry<String, Integer> e : map.entrySet()){
+		System.out.println(e.getKey()+"------->"+e.getValue());
+	}
 	}
 }
