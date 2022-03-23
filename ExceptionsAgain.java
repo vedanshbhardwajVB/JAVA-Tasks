54518f848e428bfd2fcee5a071f98ca53d8b29c2
Vedansh Bhardwaj <vedansh.ved@anywhere.co>
2022-03-23 10:34:20 +0530

    Adding the boolean flag method of ommitting Finally block

--- ExceptionsAgain.java
+++ ExceptionsAgain.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 public class ExceptionsAgain {
 	public static void main (String [] args) {
-	System.out.println("Enter 1 for Arithmetic exception and anything else for Null pointer exception");
+	System.out.println("Enter 1 for Arithmetic exception 2 for Null pointer exception and 3 for NumberFormatExceptions");
 	Scanner sc = new Scanner(System.in);
 	int choice = sc.nextInt();
-
+	boolean flag=true;
 	try {
 		if(choice == 1) {
 				int a=0;
@@ -13,10 +13,17 @@ public class ExceptionsAgain {
 				System.out.println("20/10="+" "+b/c);
 				System.out.println("20/0="+" "+b/a);
 		}
-		else {
+		else if (choice == 2) {
 				String s2=null;
 				System.out.println(s2.length());
 		}
+		else if(choice == 3) {
+				String s="vedansh";
+				int n = Integer.parseInt(s);
+		}
+
+		else 
+			System.out.println("Wrong Input");
 	}
 
 	catch (ArithmeticException e) {
@@ -25,10 +32,18 @@ public class ExceptionsAgain {
 	}
 
 	catch(NullPointerException e) {System.out.println("Exception occurred called -> "+e);}
+
+	catch(NumberFormatException e) {
+		System.out.println("Exception occurred called -> "+e);
+		flag=false;
+		}
 	
 	finally {
+		if(flag)
 		System.out.println("Try again Later");
 	}
+
+	System.out.println("ThankYou !!");
 
 	}
 }
